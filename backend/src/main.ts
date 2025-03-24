635342892330890f75b7d4ea39185355e594230e
patricksilvy <pat28910oshi@gmail.com>
2025-03-24 04:37:37 -0300

    fix cors 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,10 +11,7 @@ async function bootstrap() {
   app.use(helmet());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost/',
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true,
+    origin: "https://softpet2.vercel.app/",
   });
 
   await app.listen(process.env.PORT ?? 3000);
